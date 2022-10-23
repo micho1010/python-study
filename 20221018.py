f46f70bf0d1594d3d8a90998d6a1beb5cad28237
micho1010 <1101802085@qq.com>
2022-10-23 12:51:55 +0800

    init

---
+++ 20221018.py
@@ -0,0 +1,30 @@
+Teacher = "Mr. Morton"
+print(Teacher)
+You = "我想要养🐈"
+print(You)
+print(23 + 27)
+print("23 + 27")
+
+First = 5
+Second = 3
+print(First + Second)
+Third = First + Second
+Third
+
+MyTeacher = "大可爱"
+YourTeacher = "自学真了不起"
+print(MyTeacher + YourTeacher)
+
+Score = 7
+Score = Score + 1
+Score = Score + 1
+print(Score)
+
+Goodlucky = 445
+print("445")
+
+daysPerWeek = 7
+hourPerDay = 26
+minutesPerHour = 60
+hourPerWeek = daysPerWeek * hourPerDay
+print(hourPerWeek)
