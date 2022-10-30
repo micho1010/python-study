f9274de35c262702a2ea579a042f45e3c89da399
micho1010 <1101802085@qq.com>
2022-10-30 23:35:32 +0800

    独立的小可爱

---
+++ 20221030.py
@@ -0,0 +1,18 @@
+import time
+for i in range(10, 0, -1):
+    print(i)
+    time.sleep(5) 
+print("BLAST OFF!")
+
+
+print(*range(1, 20, 5))
+
+
+
+
+
+for letter in "hi there!":
+    print(letter)
+
+for looper in range(1, 6):
+    print(looper, "times 5 =", looper * 2)
