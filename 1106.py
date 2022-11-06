1c93e56205ecff3ee963dc2ff69a3c39f51eff05
micho1010 <1101802085@qq.com>
2022-11-06 23:22:18 +0800

    嵌套循环2

--- 1106.py
+++ 1106.py
@@ -1,7 +1,14 @@
 start = int(input("起始值："))
 end = int(input("结束值："))
 
+
 for num1 in range(start, end + 1):
     print("----------")
-    for num2 in range(1,10):
-        print(num1, "*", num2, "=", num1 * num2)
+    if start <= 5:
+        for i in range(9, 0, -1):
+            print(num1, "*", i, "=", num1 * i)
+
+    else:
+        for j in range(1,10):
+            print(num1, "*", j, "=", num1 * j)
+
