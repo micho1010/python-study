8e53756418cea871a7b1276d684474a0403ce97e
micho1010 <1101802085@qq.com>
2022-11-07 23:29:46 +0800

    12章列表

---
+++ 12list.py
@@ -0,0 +1,12 @@
+friends = []
+friends.append('David')
+friends.append('Lucy')
+friends.append('Moon')
+friendslast = friends.pop(0)
+# del friends[2]
+# friends.insert(1,'love')
+# friends.remove('Moon')
+# friends[2] = 'Love‘
+# friends.extend(['is','a','love','story'])
+print(friendslast)
+print(friends)
