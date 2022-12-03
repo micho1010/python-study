d9bed90cde254a58d0122f4751bc8ac2cfe8f3b6
micho1010 <1101802085@qq.com>
2022-12-03 22:45:23 +0800

    第二册第三单元

--- 11嵌套循环1105.py
+++ 11嵌套循环1105.py
@@ -2,27 +2,27 @@ import time
 count = int(input("你这个炸弹犯，说还有几秒钟爆炸！"))
 for i in range(count, 0, -1):
     print(i)
-    time.sleep(1) 
+    time.sleep(1)
 print("BLAST OFF!")
 
-for i in range(5): 
-for j in range(3):
-print('* ', end='') 
+for i in range(5):
+    for j in range(3):
+        print('* ', end='')
 print()
 
 
-numLines = int(input('How many lines of stars do you want? ')) 
+numLines = int(input('How many lines of stars do you want? '))
 numStars = int(input('How many stars per line? '))
 for line in range(0, numLines):
-    for star in range(0, numStars): 
+    for star in range(0, numStars):
         print('* ', end='')
     print("~~~~~~~~~~~~~~")
 
 for multiplier in range(5, 8):
     for i in range(1, 11):
-        print(i, "×", multiplier, "=", i * multiplier) 
+        print(i, "×", multiplier, "=", i * multiplier)
 
 
 num1 = int(input("数字"))
-for num2 in range(1,10):
-    print(num1, "*",num2, "=", num1 * num2)
+for num2 in range(1, 10):
+    print(num1, "*", num2, "=", num1 * num2)
---
+++ s2.py
@@ -0,0 +1,11 @@
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+# print(motorcycles[2])
+# motorcycles[2] = 'DaYun'
+# print(motorcycles)
+# motorcycles.append('HaLei')
+
+# motorcycles.insert(1, '86')
+# del motorcycles[1]
+# motorcycles.remove('honda')
+# motorcycles.pop(0)
+# print(motorcycles)
